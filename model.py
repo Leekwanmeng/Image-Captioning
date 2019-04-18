dd212acf4431e9ca0456ff22ba3fe54b94e5ef37
Leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-04-18 17:20:44 +0800

    Change encoder mode

--- model.py
+++ model.py
@@ -60,44 +60,22 @@ class DecoderRNN(nn.Module):
 
 
 
-# class NewEncoderCNN(nn.Module):
-#     def __init__(self, embed_size):
-#         """Load the pretrained ResNet-152 and replace top fc layer."""
-#         super(NewEncoderCNN, self).__init__()
-#         resnet = models.resnet152() # not pretrained
-#         modules = list(resnet.children())[:-1]      # delete the last fc layer.
-#         self.resnet = nn.Sequential(*modules)
-#         self.linear = nn.Linear(resnet.fc.in_features, embed_size)
-#         self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
-        
-#     def forward(self, images):
-#         """Extract feature vectors from input images."""
-#         features = self.resnet(images)
-#         features = features.reshape(features.size(0), -1)
-#         features = self.bn(self.linear(features))
-#         return features
-
-
-
 class Encoder(nn.Module):
-    """
-    Encoder.
-    """
-
-    def __init__(self, encoded_image_size=14):
+    def __init__(self, enc_image_size=14):
         super(Encoder, self).__init__()
-        self.enc_image_size = encoded_image_size
+        self.enc_image_size = enc_image_size
 
-        resnet = models.resnet101(pretrained=True)  # pretrained ImageNet ResNet-101
-
-        # Remove linear and pool layers (since we're not doing classification)
-        modules = list(resnet.children())[:-2]
+        resnet = models.resnet50(pretrained=True)
+        modules = list(resnet.children())[:-2] # remove fc and pool layers
         self.resnet = nn.Sequential(*modules)
-
-        # Resize image to fixed size to allow input images of variable size
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
-
-        self.fine_tune()
+        self.conv1x1 = nn.Conv2d(in_channels=2048, out_channels=512, kernel_size=1)
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((enc_image_size, enc_image_size))
+        
+        # fine-tune params in last convolutional block
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+        for param in list(self.resnet.children())[-1].parameters():
+            param.requires_grad = True
 
     def forward(self, images):
         """
@@ -106,23 +84,11 @@ class Encoder(nn.Module):
         :return: encoded images
         """
         out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
-        out = self.adaptive_pool(out)  # (batch_size, 2048, encoded_image_size, encoded_image_size)
-        out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 2048)
+        print("res out shape: ".format(out.shape))
+        out = self.adaptive_pool(out)  # (batch_size, 2048, enc_image_size, enc_image_size)
+        out = self.conv1x1(out)  # (batch_size, 512, enc_image_size, enc_image_size)
+        out = out.permute(0, 2, 3, 1)  # (batch_size, enc_image_size, enc_image_size, 512)
         return out
-
-    def fine_tune(self, fine_tune=True):
-        """
-        Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
-        :param fine_tune: Allow?
-        """
-        for p in self.resnet.parameters():
-            p.requires_grad = False
-        # If fine-tuning, only fine-tune convolutional blocks 2 through 4
-        for c in list(self.resnet.children())[5:]:
-            for p in c.parameters():
-                p.requires_grad = fine_tune
-
-
 
 
 class Attention(nn.Module):
@@ -145,7 +111,7 @@ class Attention(nn.Module):
 
 #embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20
 class DecoderWithAttention(nn.Module):
-    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=2048, dropout=0.5):
+    def __init__(self, attention_dim, embed_dim, decoder_dim, vocab_size, encoder_dim=512, dropout=0.5):
         """
         :param attention_dim: size of attention network
         :param embed_dim: embedding size
@@ -183,21 +149,6 @@ class DecoderWithAttention(nn.Module):
         self.fc.bias.data.fill_(0)
         self.fc.weight.data.uniform_(-0.1, 0.1)
 
-    def load_pretrained_embeddings(self, embeddings):
-        """
-        Loads embedding layer with pre-trained embeddings.
-        :param embeddings: pre-trained embeddings
-        """
-        self.embedding.weight = nn.Parameter(embeddings)
-
-    def fine_tune_embeddings(self, fine_tune=True):
-        """
-        Allow fine-tuning of embedding layer? (Only makes sense to not-allow if using pre-trained embeddings).
-        :param fine_tune: Allow?
-        """
-        for p in self.embedding.parameters():
-            p.requires_grad = fine_tune
-
     def init_hidden_state(self, encoder_out):
         """
         Creates the initial hidden and cell states for the decoder's LSTM based on the encoded images.
@@ -223,12 +174,11 @@ class DecoderWithAttention(nn.Module):
         vocab_size = self.vocab_size
 
         # Flatten image
-        encoder_out = encoder_out.view(batch_size, -1, encoder_dim)  # (batch_size, num_pixels, encoder_dim)
+        encoder_out = encoder_out.view(batch_size, -1, encoder_dim)
         num_pixels = encoder_out.size(1)
 
-        # Sort input data by decreasing lengths; why? apparent below
+        # Sort input data by decreasing lengths
         caption_lengths = torch.LongTensor(caption_lengths)
-#        print(caption_lengths)
         caption_lengths, sort_ind = caption_lengths.sort(dim=0, descending=True)
         encoder_out = encoder_out[sort_ind]
         encoded_captions = encoded_captions[sort_ind]
@@ -265,32 +215,17 @@ class DecoderWithAttention(nn.Module):
 
         return predictions, encoded_captions, decode_lengths, alphas, sort_ind
 
+    def sample(self, features, states=None):
+        """Generate captions for given image features using greedy search."""
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(self.max_seg_length):
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
+            outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
+        sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
+        return sampled_ids
 
-# class Attention(nn.Module):
-#     def __init__(self, encoder_dim, decoder_dim, attention_dim):
-#         """
-#         :param encoder_dim: feature size of encoded images
-#         :param decoder_dim: size of decoder's RNN
-#         :param attention_dim: size of the attention network
-#         """
-#         super(Attention, self).__init__()
-#         self.encoder_att = nn.Linear(encoder_dim, attention_dim)  # linear layer to transform encoded image
-#         self.decoder_att = nn.Linear(decoder_dim, attention_dim)  # linear layer to transform decoder's output
-#         self.full_att = nn.Linear(attention_dim, 1)  # linear layer to calculate values to be softmax-ed
-#         self.relu = nn.ReLU()
-#         self.softmax = nn.Softmax(dim=1)  # softmax layer to calculate weights
-
-#     def forward(self, encoder_out, decoder_hidden):
-#         """
-#         Forward propagation.
-#         :param encoder_out: encoded images, a tensor of dimension (batch_size, num_pixels, encoder_dim)
-#         :param decoder_hidden: previous decoder output, a tensor of dimension (batch_size, decoder_dim)
-#         :return: attention weighted encoding, weights
-#         """
-#         att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
-#         att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-#         att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
-#         alpha = self.softmax(att)  # (batch_size, num_pixels)
-#         attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
-
-#         return attention_weighted_encoding, alpha
