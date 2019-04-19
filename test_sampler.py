1a97a44276ca7a998d48ee5fdb0e9e21c700ee8a
leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-04-19 14:33:17 +0800

    add models file to github

--- test_sampler.py
+++ test_sampler.py
@@ -1,10 +1,12 @@
 from sampler import Sampler
 from build_vocab import Vocabulary
 
-model = Sampler('./models/checkpoint_1_5120.pt', './data/vocab.pkl')
+model = Sampler('./models/checkpoint_1_7142.pt', './data/vocab.pkl')
 
 # model.decoder.max_seg_length = 20
 
+seq, alpha = model.caption_image_beam_search('./sample/sample_dog.jpg')
 seq, alpha = model.caption_image_beam_search('./sample/sample_beach.jpg')
-
-print(seq)
+seq, alpha = model.caption_image_beam_search('./sample/sample_img.jpg')
+seq, alpha = model.caption_image_beam_search('./sample/sample_office.jpg')
+# print(seq)
