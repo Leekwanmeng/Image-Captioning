dacec54077724715c6a6b20228e1cd54376ca743
Leekwanmeng <kwanmeng_lee@mymail.sutd.edu.sg>
2019-04-21 02:24:54 +0800

    sample

--- sampler.py
+++ sampler.py
@@ -51,130 +51,98 @@ class Sampler(object):
         
         return base_image, sentence
 
-    def caption_image_beam_search(self, img_path, beam_size=3):
-        """
-        Reads an image and captions it with beam search.
-        :param encoder: encoder model
-        :param decoder: decoder model
-        :param image_path: path to image
-        :param self.vocab: word map
-        :param beam_size: number of sequences to consider at each decode-step
-        :return: caption, weights for visualization
-        """
 
-        k = beam_size
+    def beam_search(self, img_path, k=3):
+        sequences = [[[], 1.0]]
         vocab_size = len(self.vocab)
+        image = self.read_image(img_path)
+        start_index = self.vocab.word2idx['<start>']
+        end_index = self.vocab.word2idx['<end>']
 
+        encoder_out = self.encoder(image)
+        enc_image_size = encoder_out.size(1)
+        encoder_dim = encoder_out.size(3)
+        encoder_out = encoder_out.view(1, -1, encoder_dim)
+        encoder_out = encoder_out.expand(k, encoder_out.size(1), encoder_dim)
+
+        step_k_prev_words = torch.LongTensor([[start_index]] * k).to(self.device)
+        sequences = step_k_prev_words
+
+        top_k_scores = torch.zeros(k, 1).to(self.device)
+        sequence_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(self.device)
+
+        complete_sequence = []
+        complete_sequence_alpha = []
+        complete_sequence_scores = []
+
+        # Begin decoding
+        step = 1
+        h, c = self.decoder.init_hidden_state(encoder_out)
+
+        while True:
+            embeddings = self.decoder.embedding(step_k_prev_words).squeeze(1)
+            enc_with_attn, alpha = self.decoder.attention(encoder_out, h)
+            alpha = alpha.view(-1, enc_image_size, enc_image_size)
+            gate = F.sigmoid(self.decoder.f_beta(h))
+            enc_with_attn = gate * enc_with_attn
+            h, c = self.decoder.lstm_step(
+                torch.cat([embeddings, enc_with_attn], dim=1),
+                (h, c)
+            )
+            scores = self.decoder.fc(h)
+            scores = F.log_softmax(scores, dim=1)
+            scores = top_k_scores.expand_as(scores) + scores
+
+            if step == 1:
+                top_k_scores, top_k_idx = scores[0].topk(k, dim=0)
+            else:
+                top_k_scores, top_k_idx = scores.view(-1).topk(k, dim=0)
+
+            # Convert unrolled indices to actual indices of scores
+            prev_word_idx = top_k_idx / vocab_size
+            next_word_idx = top_k_idx % vocab_size
+
+            # Add new words to sequences, alphas
+            sequences = torch.cat([sequences[prev_word_idx], next_word_idx.unsqueeze(1)], dim=1)
+            sequence_alpha = torch.cat([sequence_alpha[prev_word_idx], alpha[prev_word_idx].unsqueeze(1)], dim=1)
+
+            incomplete_idx = [i for i, next_word in enumerate(next_word_idx) if next_word != end_index]
+            complete_idx = list(set(range(len(next_word_idx))) - set(incomplete_idx))
+
+            if len(complete_idx) > 0:
+                complete_sequence.extend(sequences[complete_idx].tolist())
+                complete_sequence_alpha.extend(sequence_alpha[complete_idx].tolist())
+                complete_sequence_scores.extend(top_k_scores[complete_idx])
+            k -= len(complete_idx)
+
+            # Proceed with incomplete sequences
+            if k == 0:
+                break
+            sequences = sequences[incomplete_idx]
+            sequence_alpha = sequence_alpha[incomplete_idx]
+            h = h[prev_word_idx[incomplete_idx]]
+            c = c[prev_word_idx[incomplete_idx]]
+            encoder_out = encoder_out[prev_word_idx[incomplete_idx]]
+            top_k_scores = top_k_scores[incomplete_idx].unsqueeze(1)
+            step_k_prev_words = next_word_idx[incomplete_idx].unsqueeze(1)
+
+            if step > 50:
+                break
+            step += 1
+
+        max_idx = complete_sequence_scores.index(max(complete_sequence_scores))
+        seq = complete_sequence[max_idx]
+        alphas = complete_sequence_alpha[max_idx]
+
+        return seq, alphas
+
+
+    def read_image(self, img_path):
         # Read image and process
         image = Image.open(img_path)
         base_image = image
         if self.transform is not None:
             image = self.transform(image).unsqueeze(0)
         image = image.to(self.device)
+        return image
 
-        # Encode
-        encoder_out = self.encoder(image)  # (1, enc_image_size, enc_image_size, encoder_dim)
-        enc_image_size = encoder_out.size(1)
-        encoder_dim = encoder_out.size(3)
-
-        # Flatten encoding
-        encoder_out = encoder_out.view(1, -1, encoder_dim)  # (1, num_pixels, encoder_dim)
-        num_pixels = encoder_out.size(1)
-
-        # We'll treat the problem as having a batch size of k
-        encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)  # (k, num_pixels, encoder_dim)
-
-        # Tensor to store top k previous words at each step; now they're just <start>
-        k_prev_words = torch.LongTensor([[self.vocab.word2idx['<start>']]] * k).to(self.device)  # (k, 1)
-
-        # Tensor to store top k sequences; now they're just <start>
-        seqs = k_prev_words  # (k, 1)
-
-        # Tensor to store top k sequences' scores; now they're just 0
-        top_k_scores = torch.zeros(k, 1).to(self.device)  # (k, 1)
-
-        # Tensor to store top k sequences' alphas; now they're just 1s
-        seqs_alpha = torch.ones(k, 1, enc_image_size, enc_image_size).to(self.device)  # (k, 1, enc_image_size, enc_image_size)
-
-        # Lists to store completed sequences, their alphas and scores
-        complete_seqs = list()
-        complete_seqs_alpha = list()
-        complete_seqs_scores = list()
-
-        # Start decoding
-        step = 1
-        h, c = self.decoder.init_hidden_state(encoder_out)
-
-        # s is a number less than or equal to k, because sequences are removed from this process once they hit <end>
-        while True:
-
-            embeddings = self.decoder.embedding(k_prev_words).squeeze(1)  # (s, embed_dim)
-
-            awe, alpha = self.decoder.attention(encoder_out, h)  # (s, encoder_dim), (s, num_pixels)
-
-            alpha = alpha.view(-1, enc_image_size, enc_image_size)  # (s, enc_image_size, enc_image_size)
-
-            gate = self.decoder.sigmoid(self.decoder.f_beta(h))  # gating scalar, (s, encoder_dim)
-            awe = gate * awe
-
-            h, c = self.decoder.decode_step(torch.cat([embeddings, awe], dim=1), (h, c))  # (s, decoder_dim)
-
-            scores = self.decoder.fc(h)  # (s, vocab_size)
-            scores = F.log_softmax(scores, dim=1)
-
-            # Add
-            scores = top_k_scores.expand_as(scores) + scores  # (s, vocab_size)
-
-            # For the first step, all k points will have the same scores (since same k previous words, h, c)
-            if step == 1:
-                top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
-            else:
-                # Unroll and find top scores, and their unrolled indices
-                top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)  # (s)
-
-            # Convert unrolled indices to actual indices of scores
-            prev_word_inds = top_k_words / vocab_size  # (s)
-            next_word_inds = top_k_words % vocab_size  # (s)
-
-            # Add new words to sequences, alphas
-            seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)  # (s, step+1)
-            seqs_alpha = torch.cat([seqs_alpha[prev_word_inds], alpha[prev_word_inds].unsqueeze(1)],
-                                dim=1)  # (s, step+1, enc_image_size, enc_image_size)
-
-            # Which sequences are incomplete (didn't reach <end>)?
-            incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
-                            next_word != self.vocab.word2idx['<end>']]
-            complete_inds = list(set(range(len(next_word_inds))) - set(incomplete_inds))
-
-            # Set aside complete sequences
-            if len(complete_inds) > 0:
-                complete_seqs.extend(seqs[complete_inds].tolist())
-                complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
-                complete_seqs_scores.extend(top_k_scores[complete_inds])
-            k -= len(complete_inds)  # reduce beam length accordingly
-
-            # Proceed with incomplete sequences
-            if k == 0:
-                break
-            seqs = seqs[incomplete_inds]
-            seqs_alpha = seqs_alpha[incomplete_inds]
-            h = h[prev_word_inds[incomplete_inds]]
-            c = c[prev_word_inds[incomplete_inds]]
-            encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
-            top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
-            k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
-
-            # Break if things have been going on too long
-            if step > 50:
-                break
-            step += 1
-
-        i = complete_seqs_scores.index(max(complete_seqs_scores))
-        seq = complete_seqs[i]
-        alphas = complete_seqs_alpha[i]
-
-        sentence = ""
-        for i in seq[1:-1]:
-            sentence += " " + self.vocab.idx2word[i]
-        print(sentence)
-        return seq, alphas
--- test_sampler.py
+++ test_sampler.py
@@ -6,9 +6,9 @@ model = Sampler('./models/checkpoint_1_7149.pt', './data/vocab.pkl')
 
 # model.decoder.max_seg_length = 20
 
-seq, alpha = model.caption_image_beam_search('./sample/sample_dog.jpg')
-seq, alpha = model.caption_image_beam_search('./sample/sample_beach.jpg')
-seq, alpha = model.caption_image_beam_search('./sample/sample_img.jpg')
-seq, alpha = model.caption_image_beam_search('./sample/sample_office.jpg')
+seq, alpha = model.beam_search('./sample/sample_dog.jpg')
+seq, alpha = model.beam_search('./sample/sample_beach.jpg')
+seq, alpha = model.beam_search('./sample/sample_img.jpg')
+seq, alpha = model.beam_search('./sample/sample_office.jpg')
 # print(seq)
 
