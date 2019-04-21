53101bcf05fee9e3111217ebab9783a4a113f3ad
mokjunnneng <mokjunnneng@gmail.com>
2019-04-21 15:49:11 +0800

    removed torch from app_requirements

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -3,5 +3,3 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess-it"
     UPLOADED_PHOTOS_DEST = os.path.join(os.getcwd(), "uploads/")
-    MODELS_FOLDER = os.path.join(os.getcwd(), "dl_models/")
-    DATA_FOLDER = os.path.join(os.getcwd(), "dl_models/")
